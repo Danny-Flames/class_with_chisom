f39357fe7e45a34b1951682d757f98ab2efc4e94
DaNny <danniskolobosiky@gmail.com>
2023-02-28 16:41:18 +0100

    latest changes

---
+++ first_class/app.js
@@ -0,0 +1,28 @@
+
+// Variables - are like containers used to store data. These data can be anything
+// In javascript, you 'declare' a variable using these keywords:  var, let, const
+
+var myName = "Daniel";
+let myName2 = "Chisom";
+const myBiography = "My name is emmanuel and i am 10yrs old. I live in Lagos, Nigeria.";
+
+console.log(myName)
+console.log(myName2)
+console.log(myBiography)
+
+// Data type - string, number, float, boolean, undefined, array, object
+const myChurch = "Foursquare Gospel Church";   // String - any text inside single or double quotes.
+var myAge = 12;   // Number
+let hisAge = "14"   // String
+let carSize = 25.67   // Float - any decimal number/value
+var isChristian = true;   // Boolean - true or false
+let favoriteFruits = ["Apple", "Orange", "Pineapple", "Tangerine", 40, false, 45.78];   // Array - list of data in square brackets -> []
+let myFrnd = {
+    name: "Peter",
+    age: 10,
+    address: "12A George Bush Crescent"
+}                               // Obj - key and value pairs in curly braces => { key: value }
+
+console.log(favoriteFruits)
+console.log(myFrnd)
+
